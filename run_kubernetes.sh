cc2aaf51d46fb8dab27b35e4e3043a41976b5a59
werner-oscar <wernertsekumah@amalitech.org>
2022-07-21 15:46:35 +0000

    added required changes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -1,18 +1,15 @@
 #!/usr/bin/env bash
-
 # This tags and uploads an image to Docker Hub
-
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-
+dockerpath=werneroscar/udacity-project-4
 # Step 2
 # Run the Docker Hub container with kubernetes
-
-
+kubectl apply -f project4.yml
 # Step 3:
 # List kubernetes pods
-
+kubectl get pods
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward pod/microservice 8089:80
